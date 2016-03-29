4d1799e92b3f311680a7a92010ac34af2f18a194
Andrew Minion <andrew@andrewrminion.com>
2016-03-29 11:54:59 -0400

    set up grunt workflow

---
+++ gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    // Watch task config
+    watch: {
+        javascript: {
+            files: ["js/*.js", "!js/*.min.js"],
+            tasks: ['uglify'],
+        },
+    },
+    uglify: {
+        custom: {
+            files: {
+                'js/initializeMap.min.js': ['js/initializeMap.js'],
+                'js/backend.min.js': ['js/backend.js'],
+            },
+        },
+    },
+    browserSync: {
+        dev: {
+            bsFiles: {
+                src : ['**/*.css', '**/*.php', '**/*.js', '!node_modules'],
+            },
+            options: {
+                watchTask: true,
+                proxy: "http://wwntbm.wordpress.dev",
+            },
+        },
+    },
+  });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.registerTask('default', [
+        'browserSync',
+        'watch',
+    ]);
+};
